89bc7e99ae449fdb8c7a7d911a4b5b9e44223155
surendra singh <surendra.singh@crossml.com>
2025-10-24 11:09:13 +0530

    Serializer subclasses and Aggregated API data

--- api/serializers.py
+++ api/serializers.py
@@ -55,3 +55,9 @@ class OrderSerializer(serializers.ModelSerializer):
             'items',
             'total_price'
         )
+
+class ProductInfoSerializer(serializers.Serializer):
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
+
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Max
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -21,4 +22,14 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price' : products.aggregate(max_price=Max('price'))['max_price']
+    })
     return Response(serializer.data)
